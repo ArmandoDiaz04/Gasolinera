9be99be9a8b7206e7ec1e53c9e333654548692f6
Juan92A <acunajuan429@gmail.com>
2023-04-08 22:29:01 -0600

    llamada de clientes

--- GasolineraDos/FormsEmpleado/Form1.Designer.cs
+++ GasolineraDos/FormsEmpleado/Form1.Designer.cs
@@ -71,7 +71,7 @@
             this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
             this.panelSideMenu.Margin = new System.Windows.Forms.Padding(2);
             this.panelSideMenu.Name = "panelSideMenu";
-            this.panelSideMenu.Size = new System.Drawing.Size(261, 781);
+            this.panelSideMenu.Size = new System.Drawing.Size(304, 749);
             this.panelSideMenu.TabIndex = 0;
             // 
             // PanelCRUD
@@ -81,9 +81,10 @@
             this.PanelCRUD.Controls.Add(this.button1);
             this.PanelCRUD.Controls.Add(this.btnClientes);
             this.PanelCRUD.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelCRUD.Location = new System.Drawing.Point(0, 444);
+            this.PanelCRUD.Location = new System.Drawing.Point(0, 511);
+            this.PanelCRUD.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PanelCRUD.Name = "PanelCRUD";
-            this.PanelCRUD.Size = new System.Drawing.Size(261, 121);
+            this.PanelCRUD.Size = new System.Drawing.Size(304, 140);
             this.PanelCRUD.TabIndex = 7;
             // 
             // button2
@@ -92,12 +93,13 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.Color.LightGray;
-            this.button2.Location = new System.Drawing.Point(0, 80);
+            this.button2.Location = new System.Drawing.Point(0, 92);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.button2.Size = new System.Drawing.Size(261, 40);
+            this.button2.Padding = new System.Windows.Forms.Padding(41, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(304, 46);
             this.button2.TabIndex = 2;
             this.button2.Text = "Eliminar Cliente";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -110,17 +112,17 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.LightGray;
-            this.button1.Location = new System.Drawing.Point(0, 40);
+            this.button1.Location = new System.Drawing.Point(0, 46);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.button1.Size = new System.Drawing.Size(261, 40);
+            this.button1.Padding = new System.Windows.Forms.Padding(41, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(304, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Editar Cliente";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.UseVisualStyleBackColor = true;
-           
             // 
             // btnClientes
             // 
@@ -128,30 +130,31 @@
             this.btnClientes.FlatAppearance.BorderSize = 0;
             this.btnClientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClientes.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnClientes.ForeColor = System.Drawing.Color.LightGray;
             this.btnClientes.Location = new System.Drawing.Point(0, 0);
+            this.btnClientes.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnClientes.Name = "btnClientes";
-            this.btnClientes.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnClientes.Size = new System.Drawing.Size(261, 40);
+            this.btnClientes.Padding = new System.Windows.Forms.Padding(41, 0, 0, 0);
+            this.btnClientes.Size = new System.Drawing.Size(304, 46);
             this.btnClientes.TabIndex = 0;
             this.btnClientes.Text = "Añadir Cliente";
             this.btnClientes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnClientes.UseVisualStyleBackColor = true;
-            
+            this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
             // 
             // btnGestionar
             // 
             this.btnGestionar.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnGestionar.FlatAppearance.BorderSize = 0;
             this.btnGestionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGestionar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGestionar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnGestionar.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnGestionar.Location = new System.Drawing.Point(0, 403);
+            this.btnGestionar.Location = new System.Drawing.Point(0, 464);
             this.btnGestionar.Margin = new System.Windows.Forms.Padding(2);
             this.btnGestionar.Name = "btnGestionar";
-            this.btnGestionar.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnGestionar.Size = new System.Drawing.Size(261, 41);
+            this.btnGestionar.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnGestionar.Size = new System.Drawing.Size(304, 47);
             this.btnGestionar.TabIndex = 6;
             this.btnGestionar.Text = "Gestionar Clientes";
             this.btnGestionar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -164,9 +167,10 @@
             this.PanelFrecuente.Controls.Add(this.btnCanjear);
             this.PanelFrecuente.Controls.Add(this.btnAcumular);
             this.PanelFrecuente.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelFrecuente.Location = new System.Drawing.Point(0, 314);
+            this.PanelFrecuente.Location = new System.Drawing.Point(0, 361);
+            this.PanelFrecuente.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PanelFrecuente.Name = "PanelFrecuente";
-            this.PanelFrecuente.Size = new System.Drawing.Size(261, 89);
+            this.PanelFrecuente.Size = new System.Drawing.Size(304, 103);
             this.PanelFrecuente.TabIndex = 5;
             // 
             // btnCanjear
@@ -175,12 +179,13 @@
             this.btnCanjear.FlatAppearance.BorderSize = 0;
             this.btnCanjear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnCanjear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCanjear.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCanjear.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnCanjear.ForeColor = System.Drawing.Color.LightGray;
-            this.btnCanjear.Location = new System.Drawing.Point(0, 40);
+            this.btnCanjear.Location = new System.Drawing.Point(0, 46);
+            this.btnCanjear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnCanjear.Name = "btnCanjear";
-            this.btnCanjear.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnCanjear.Size = new System.Drawing.Size(261, 40);
+            this.btnCanjear.Padding = new System.Windows.Forms.Padding(41, 0, 0, 0);
+            this.btnCanjear.Size = new System.Drawing.Size(304, 46);
             this.btnCanjear.TabIndex = 1;
             this.btnCanjear.Text = "Canjear Puntos";
             this.btnCanjear.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -193,12 +198,13 @@
             this.btnAcumular.FlatAppearance.BorderSize = 0;
             this.btnAcumular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnAcumular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAcumular.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcumular.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnAcumular.ForeColor = System.Drawing.Color.LightGray;
             this.btnAcumular.Location = new System.Drawing.Point(0, 0);
+            this.btnAcumular.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnAcumular.Name = "btnAcumular";
-            this.btnAcumular.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnAcumular.Size = new System.Drawing.Size(261, 40);
+            this.btnAcumular.Padding = new System.Windows.Forms.Padding(41, 0, 0, 0);
+            this.btnAcumular.Size = new System.Drawing.Size(304, 46);
             this.btnAcumular.TabIndex = 0;
             this.btnAcumular.Text = "Acumular Puntos";
             this.btnAcumular.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -210,13 +216,13 @@
             this.btnFrecuentes.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnFrecuentes.FlatAppearance.BorderSize = 0;
             this.btnFrecuentes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFrecuentes.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFrecuentes.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnFrecuentes.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnFrecuentes.Location = new System.Drawing.Point(0, 273);
+            this.btnFrecuentes.Location = new System.Drawing.Point(0, 314);
             this.btnFrecuentes.Margin = new System.Windows.Forms.Padding(2);
             this.btnFrecuentes.Name = "btnFrecuentes";
-            this.btnFrecuentes.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnFrecuentes.Size = new System.Drawing.Size(261, 41);
+            this.btnFrecuentes.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnFrecuentes.Size = new System.Drawing.Size(304, 47);
             this.btnFrecuentes.TabIndex = 4;
             this.btnFrecuentes.Text = "Cliente Frecuente";
             this.btnFrecuentes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -228,9 +234,10 @@
             this.panelVenta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(29)))));
             this.panelVenta.Controls.Add(this.btnFactura);
             this.panelVenta.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelVenta.Location = new System.Drawing.Point(0, 219);
+            this.panelVenta.Location = new System.Drawing.Point(0, 252);
+            this.panelVenta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panelVenta.Name = "panelVenta";
-            this.panelVenta.Size = new System.Drawing.Size(261, 54);
+            this.panelVenta.Size = new System.Drawing.Size(304, 62);
             this.panelVenta.TabIndex = 3;
             this.panelVenta.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -240,12 +247,13 @@
             this.btnFactura.FlatAppearance.BorderSize = 0;
             this.btnFactura.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnFactura.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnFactura.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFactura.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnFactura.ForeColor = System.Drawing.Color.LightGray;
             this.btnFactura.Location = new System.Drawing.Point(0, 0);
+            this.btnFactura.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnFactura.Name = "btnFactura";
-            this.btnFactura.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnFactura.Size = new System.Drawing.Size(261, 40);
+            this.btnFactura.Padding = new System.Windows.Forms.Padding(41, 0, 0, 0);
+            this.btnFactura.Size = new System.Drawing.Size(304, 46);
             this.btnFactura.TabIndex = 0;
             this.btnFactura.Text = "Generar Factura";
             this.btnFactura.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -257,13 +265,13 @@
             this.btnVenta.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnVenta.FlatAppearance.BorderSize = 0;
             this.btnVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnVenta.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVenta.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnVenta.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnVenta.Location = new System.Drawing.Point(0, 178);
+            this.btnVenta.Location = new System.Drawing.Point(0, 205);
             this.btnVenta.Margin = new System.Windows.Forms.Padding(2);
             this.btnVenta.Name = "btnVenta";
-            this.btnVenta.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
-            this.btnVenta.Size = new System.Drawing.Size(261, 41);
+            this.btnVenta.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnVenta.Size = new System.Drawing.Size(304, 47);
             this.btnVenta.TabIndex = 2;
             this.btnVenta.Text = "Venta";
             this.btnVenta.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -277,15 +285,16 @@
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
             this.panelLogo.Margin = new System.Windows.Forms.Padding(2);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(261, 178);
+            this.panelLogo.Size = new System.Drawing.Size(304, 205);
             this.panelLogo.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(261, 178);
+            this.pictureBox1.Size = new System.Drawing.Size(304, 205);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -295,31 +304,33 @@
             this.panelContenedor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
             this.panelContenedor.Controls.Add(this.pictureBox2);
             this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContenedor.Location = new System.Drawing.Point(261, 0);
+            this.panelContenedor.Location = new System.Drawing.Point(304, 0);
+            this.panelContenedor.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.panelContenedor.Name = "panelContenedor";
-            this.panelContenedor.Size = new System.Drawing.Size(1299, 781);
+            this.panelContenedor.Size = new System.Drawing.Size(1066, 749);
             this.panelContenedor.TabIndex = 1;
             this.panelContenedor.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContenedor_Paint);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(240, 34);
+            this.pictureBox2.Location = new System.Drawing.Point(280, 39);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(866, 685);
+            this.pictureBox2.Size = new System.Drawing.Size(1010, 790);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1560, 781);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
             this.Controls.Add(this.panelContenedor);
             this.Controls.Add(this.panelSideMenu);
             this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(1560, 781);
+            this.MinimumSize = new System.Drawing.Size(1364, 726);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- GasolineraDos/FormsEmpleado/Form1.cs
+++ GasolineraDos/FormsEmpleado/Form1.cs
@@ -18,7 +18,15 @@ namespace Gasolinera
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            var screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            var screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            var scaleFactor = Math.Min((double)screenWidth / 1920, (double)screenHeight / 1080);
+            this.Size = new Size((int)(1920 * scaleFactor), (int)(1080 * scaleFactor));
 
+            this.TopMost = true; // El formulario se muestra siempre en primer plano
+            this.Font = new Font(this.Font.FontFamily, (float)(this.Font.Size * scaleFactor));
+            ///configura la posicion segun tamaño
+            this.Location = new Point((screenWidth - this.Width) / 2, (screenHeight - this.Height) / 2);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e) {
@@ -115,6 +123,11 @@ namespace Gasolinera
 
         }
 
-       
+        private void btnClientes_Click(object sender, EventArgs e)
+        {
+            abrirFormularios(new frmGestionarCliente());
+            //dejar hasta abajo por que este cierra los submenu :P
+            ocultarSubMenus();
+        }
     }
 }
